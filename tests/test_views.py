14632c8a346bc6beb9a4aabf0f330e08ba1ad329
florent <florent@localhost.localdomain>
2019-04-23 10:55:09 +0200

    Ré organisation du projet

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,4 +1,8 @@
 from gpbapp.views import index
 
 def test_la_fonction_index_de_views():
+    """
+    Premier test après avoir installer Flask
+    Sert à vérifier que Flask est bien installer correctement
+    """
     assert index() == "Hello world !"
