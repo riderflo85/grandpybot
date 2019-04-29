2fd76a6b2da3031e0035b045d208376fec2aa756
florent <florent@localhost.localdomain>
2019-04-29 17:30:58 +0200

    Ajout de constant.py, création du parser ainsi que de son test

--- gpbapp/views.py
+++ gpbapp/views.py
@@ -3,12 +3,68 @@
 
 from flask import Flask, render_template
 
+from . import constant as const
+
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index/')
 def index():
+    print(type(render_template('index.html')))
     return render_template('index.html')
+
+def parser(phrase_a_parser):
+    """
+    Fonction qui découpe une chaine de caractère en une liste de mots
+    et sans ponctuation
+    """
+    result = phrase_a_parser.split()
+    phrase_temp = []
+    complete_phrase = []
+
+    nb = len(const.SIGN_LIST)
+
+    loop = nb - nb
+    for s in const.SIGN_LIST:
+        sign = s
+        
+        if loop == 0:
+            for i in result:
+                if sign in i:
+                    second_parse = i.split(sign)
+                    for w in second_parse:
+                        complete_phrase.append(w)
+                else:
+                    complete_phrase.append(i)
+            phrase_temp = complete_phrase
+            loop += 1
+
+        else:
+            for i in complete_phrase:
+                if sign in i:
+                    second_parse = i.split(sign)
+                    for w in second_parse:
+                        phrase_temp.append(w)
+                else:
+                    phrase_temp.append(i)
+        
+        if loop < nb:
+            complete_phrase = []
+            complete_phrase = phrase_temp
+            phrase_temp = []
+            loop += 1
+        
+        else:
+            complete_phrase = phrase_temp
+            phrase_temp = []
+
+    for e in complete_phrase[:]:
+        if e == "":
+            complete_phrase.remove(e)
+
+    return complete_phrase
+
+    
 
 if __name__ == "__main__":
     app.run()
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,11 +1,23 @@
 #! /usr/bin/env python
 # coding: utf-8
 
-from gpbapp.views import index
+from gpbapp.views import index, parser
 
-def test_la_fonction_index_de_views():
+# def test_la_fonction_index_de_views():
+#     """
+#     Premier test après avoir installer Flask
+#     Sert à vérifier que Flask est bien installer correctement
+#     """
+#     assert index() == "Hello world !"
+
+# def test_la_fonction_index_de_views():
+    
+#     assert index() == str
+
+def test_la_fonction_parser():
     """
-    Premier test après avoir installer Flask
-    Sert à vérifier que Flask est bien installer correctement
+    La fonction parser retourne une liste de mots sans ponctuation
     """
-    assert index() == "Hello world !"
+    phrase_a_parser = "Bonjour grandpy, peut tu me trouver l'adresse d'openclassrooms? merci mon pote, je t'aime bien l'ami"
+
+    assert parser(phrase_a_parser) == ["Bonjour", "grandpy", "peut", "tu", "me", "trouver", "l", "adresse", "d", "openclassrooms", "merci", "mon", "pote", "je", "t", "aime", "bien", "l", "ami"]
