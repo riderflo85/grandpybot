0c3904ecde226cc385e13daeffa4ea1202cf63b7
florent <florent@localhost.localdomain>
2019-05-03 19:30:41 +0200

    Ajout de l'image du papy qui réfléchis et ajout d'une grille bootstrap dans le header de la conv (HTML)

--- gpbapp/static/js/script.js
+++ gpbapp/static/js/script.js
@@ -63,9 +63,6 @@ $(document).ready(function () {
     //     }
     // }
 
-
-
-    console.log('Page prete')
     var data = $('#champsDeSaisie');
     var emojiTired = "<i class='far fa-tired'></i>";
     var emojiGrinWink = "<i class='far fa-grin-wink'></i>";
@@ -80,68 +77,70 @@ $(document).ready(function () {
         if (event.key === "Enter") {
 
             var textUser = data.val();
-            var addBaliseUser = "<p class='d-none'>"+textUser+'</p>';
-            
-            data.val("");
-            $(addBaliseUser).appendTo('#cardBody');
+            if (textUser !== "") {
+                var addBaliseUser = "<p class='d-none'>"+textUser+'</p>';
 
-            var lastTextUser = $('#cardBody > p:last');
+                data.val("");
+                $(addBaliseUser).appendTo('#cardBody');
 
-            lastTextUser.addClass(classUser);
-            lastTextUser.removeClass('d-none');
-            scrollElem.scrollTop = scrollElem.scrollHeight;
-            // var intervalId = setInterval(ClignotementImg, 2000);
+                var lastTextUser = $('#cardBody > p:last');
 
-            $.ajax({
-                url: '/search',
-                type: 'GET',
-                data: 'phrase=' + textUser,
-                success: function (response) {
+                lastTextUser.addClass(classUser);
+                lastTextUser.removeClass('d-none');
+                scrollElem.scrollTop = scrollElem.scrollHeight;
+                // var intervalId = setInterval(ClignotementImg, 2000);
 
-                    /* Response Ajax */
-                    console.log(response);
-                    var reponseAjax = JSON.parse(response);
-                    var informations;
-                    var check;
-                    var history = reponseAjax.history;
-                    console.log(history);
-                    /* end response Ajax */
+                $.ajax({
+                    url: '/search',
+                    type: 'GET',
+                    data: 'phrase=' + textUser,
+                    success: function (response) {
 
-                    if (reponseAjax.result === "Lieu non trouver") {
-                        informations = {'formatted_address': 'Aucune adresse ne correspond', 'name': 'ta demande'};
-                        check = false;
-                    }else {
-                        informations = reponseAjax.result;
-                        var coordinate = informations.location;
-                        check = true;
-                    }
+                        /* Response Ajax */
+                        var reponseAjax = JSON.parse(response);
+                        var informations;
+                        var check;
+                        var history = reponseAjax.history;
+                        console.log(history);
+                        /* end response Ajax */
 
-                    if (history === "La page n'existe pas") {
-                        if (check) {
-                            textBot1 = "Voici l'adresse que tu ma demander gamins<br>"+informations.formatted_address+"<br>"+emojiGrinWink;
-                            firstMsgBot(textBot1, maps=true, coord=coordinate);
-                            textBot2 = "Je suis désoler, j'ai pas réussi à trouver d'histoire sur "+informations.name+".<br>Soit il est trop vieux et mes aventures ne mi conduisent pas, ou soit il est trop récent pour moi "+emojiTired;
-                            secondMsgBot(textBot2);
-                        }else{
-                            textBot1 = "Tu m'excuseras mais GrandPy n'a pas réussi à trouver d'adresse pour "+informations.name+". "+emojiTired;
-                            firstMsgBot(textBot1);
-                            textBot2 = "Je suis désoler, j'ai pas réussi à trouver d'histoire sur "+informations.name+".<br>Soit il est trop vieux et mes aventures ne mi conduisent pas, ou soit il est trop récent pour moi "+emojiTired;
-                            secondMsgBot(textBot2);
-
+                        if (reponseAjax.result === "Lieu non trouver") {
+                            informations = {'formatted_address': 'Aucune adresse ne correspond', 'name': 'ta demande'};
+                            check = false;
+                        }else {
+                            informations = reponseAjax.result;
+                            var coordinate = informations.location;
+                            check = true;
                         }
-                    }else {
-                        textBot1 = "Voici l'adresse que tu ma demander gamins<br>"+informations.formatted_address+emojiGrinWink;
-                        firstMsgBot(textBot1, maps=true, coord=coordinate);
-                        textBot2 = "Mon petit voici l'histoire de "+informations.name+".<br>"+history.description+"<br>Si tu veut en savoir plus sur ce lieu, vas voir ici <a href=\""+history.link+"\" class='badge badge-info'>wikipedia</a>";
-                        secondMsgBot(textBot2);
-                    }
 
-                    scrollElem.scrollTop = scrollElem.scrollHeight;
-                },
-                error: function (error) {
-                    console.log(error);
+                        if (history === "La page n'existe pas") {
+                            if (check) {
+                                textBot1 = "Voici l'adresse que tu ma demander gamins<br>"+informations.formatted_address+"<br>"+emojiGrinWink;
+                                firstMsgBot(textBot1, maps=true, coord=coordinate);
+                                textBot2 = "Je suis désoler, j'ai pas réussi à trouver d'histoire sur "+informations.name+".<br>Soit il est trop vieux et mes aventures ne mi conduisent pas, ou soit il est trop récent pour moi "+emojiTired;
+                                secondMsgBot(textBot2);
+                            }else{
+                                textBot1 = "Tu m'excuseras mais GrandPy n'a pas réussi à trouver d'adresse pour "+informations.name+". "+emojiTired;
+                                firstMsgBot(textBot1);
+                                textBot2 = "Je suis désoler, j'ai pas réussi à trouver d'histoire sur "+informations.name+".<br>Soit il est trop vieux et mes aventures ne mi conduisent pas, ou soit il est trop récent pour moi "+emojiTired;
+                                secondMsgBot(textBot2);
+
+                            }
+                        }else {
+                            textBot1 = "Voici l'adresse que tu ma demander gamins<br>"+informations.formatted_address+emojiGrinWink;
+                            firstMsgBot(textBot1, maps=true, coord=coordinate);
+                            textBot2 = "Mon petit voici l'histoire de "+informations.name+".<br>"+history.description+"<br>Si tu veut en savoir plus sur ce lieu, vas voir ici <a href=\""+history.link+"\" class='badge badge-info'>wikipedia</a>";
+                            secondMsgBot(textBot2);
+                        }
+
+                        scrollElem.scrollTop = scrollElem.scrollHeight;
+                    },
+                    error: function (error) {
+                        console.log(error);
+                    }
+                });
                 }
-            });
+            
             scrollElem.scrollTop = scrollElem.scrollHeight;
         }
     });
